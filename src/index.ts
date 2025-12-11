d94be85560fa7f6ab9eaf884b957ea87086f3384
jakub.hana <jakub.hana@newtonmedia.cz>
2025-12-11 21:23:17 +0100

    Functional getting of token

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,19 @@
 ﻿
-console.log('Test')
+// Docs - https://github.com/bakalari-api/bakalari-api-v3
+
+const API_URL = 'https://is.sssvt.cz/';
+
+// Fetch login from Bakalari API
+const params = new URLSearchParams();
+params.append('username', 'hana');
+params.append('password', '');
+params.append('grant_type', 'password');
+params.append('client_id', 'ANDR');
+
+const response = await fetch(`${API_URL}/IS/api/login`, {
+    method: 'POST',
+    body: params
+});
+
+const data = await response.json()
+console.log(data)
