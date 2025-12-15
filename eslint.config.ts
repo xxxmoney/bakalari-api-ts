5a183f048cdacd5348d0cd8e4135d9ff410ecea9
xxxmoney <xxxmoney111@gmail.com>
2025-12-15 14:30:47 +0100

    Add rules to eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,14 +1,36 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
 
 export default defineConfig([
-  {
-    files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    languageOptions: { globals: globals.browser }
-  },
-  tseslint.configs.recommended,
+    // Predefined rules
+    tseslint.configs.recommended,
+    tseslint.configs.stylistic,
+    // Config and rules
+    {
+        files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+        plugins: { js },
+        extends: ['js/recommended'],
+        languageOptions: { globals: globals.browser },
+        rules: {
+            // Single quotes (using the base 'quotes' rule)
+            'quotes': ['error', 'single', { 'avoidEscape': true }],
+
+            // Semicolons (using the base 'semi' rule)
+            'semi': ['error', 'always'],
+
+            // 4-space indentation (using the base 'indent' rule)
+            'indent': ['error', 4, { 'SwitchCase': 1 }],
+
+            // Spacing around keywords and commas, and inside object literals
+            'keyword-spacing': 'error',
+
+            // Spaces after commas
+            'comma-spacing': 'error',
+
+            // Spaces inside curly braces of object
+            'object-curly-spacing': ['error', 'always'],
+        }
+    }
 ]);
