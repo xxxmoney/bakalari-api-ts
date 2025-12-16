e6f0caaee9748bfbb6363644ef5769fd726a559a
jakub.hana <jakub.hana@newtonmedia.cz>
2025-12-16 13:25:35 +0100

    More refactor, improvements

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,4 @@
-﻿// Define the "barrel file" - simplify imports for consumer of the package
-
-// Export models
-import { TimetableService } from './services/timetable.service';
-import { Api } from './api/base.api';
-import type { Credentials } from './models/credentials.model';
-import { LoginService } from './services/login.service';
+﻿// Which APIs should be provided to user of the package
 
 export * from './models/login.model';
 
@@ -14,21 +8,4 @@ export * from './models/timetable-response.model';
 
 export * from './models/timetable-summary.model';
 
-// Export services as single unified service
-export class BakalariClient {
-    private readonly login: LoginService;
-
-    public readonly timetable: TimetableService;
-
-    constructor(baseUrl: string, credentials: Credentials) {
-        const api = new Api(baseUrl);
-
-        this.login = new LoginService(api, credentials);
-
-        this.timetable = new TimetableService(api);
-    }
-
-    async initialize() {
-        return this.login.authenticate();
-    }
-}
+export * from './bakalari.client';
